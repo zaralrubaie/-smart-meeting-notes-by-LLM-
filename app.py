72d1e6a2cf8c2ca6d6aa236e8fd79aec78c861df
zahraa <167252881+zaralrubaie@users.noreply.github.com>
2025-09-08 23:00:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-
+# import needed libraries
 import gradio as gr
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-
-tokenizer = AutoTokenizer.from_pretrained("sshleifer/distilbart-cnn-12-6")
+#  Initialize tokenizer and model
+# Tokenizer converts text into a numerical representation that the model can understand
 model = AutoModelForSeq2SeqLM.from_pretrained("sshleifer/distilbart-cnn-12-6")
-
 def summarize_text(text):
-    inputs = tokenizer(text, return_tensors="pt", max_length=1024, truncation=True)
-    summary_ids = model.generate(
+ # Tokenize input text
+ inputs = tokenizer(text, return_tensors="pt", max_length=1024, truncation=True)
+# Generate summary
+ summary_ids = model.generate(
         inputs["input_ids"],
         attention_mask=inputs["attention_mask"],
         max_length=100,
@@ -17,9 +18,10 @@ def summarize_text(text):
         early_stopping=True,
         no_repeat_ngram_size=3
     )
+# decode will help to represent the ouptput as human understand language
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return summary
-
+#  Launch Gradio Interface for Interactive Summarization
 iface = gr.Interface(
     fn=summarize_text,
     inputs=gr.Textbox(lines=5, label="Input Text"),
@@ -27,5 +29,6 @@ iface = gr.Interface(
     title="DistilBART Summarizer",
     description="Summarize any input text using DistilBART fine-tuned model."
 )
+# Start the web interface
 
 iface.launch()
